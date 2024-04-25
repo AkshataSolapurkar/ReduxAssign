14dacf5afa353f0f64bc16d83a9f4afaca16e31a
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-04-25 09:09:33 +0530

    Alomst done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './component/Addtodo'
+import Todos from './component/todos'
 
 function App() {
 
   return (
     <>
       <div>lEARN Reduxtool kit</div>
+    <AddTodo/>
+    <Todos/>
     </>
   )
 }
